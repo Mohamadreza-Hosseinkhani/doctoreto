c9294464c70cbdbedc01ba608cd5fc7c7b888335
Mohamadreza Hosseinkhani <mohamadreza.webdeveloper@gmail.com>
2025-01-30 01:30:16 +0330

    Add app.js and Swiper.js library
    - app.js was created as the main JavaScript file to handle the import of other JS files
    - Integrated Swiper.js library into the project for enhanced functionality

---
+++ src/javascript/swiper.js
@@ -0,0 +1,6 @@
+import Swiper from "swiper";
+import "swiper/css";
+
+// config your swiper here and export it. for example: specialties , disease, user view, etc... sections
+export const specialties = new Swiper();
+export const disease = new Swiper();
